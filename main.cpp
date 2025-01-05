ad1d741681912a18c69f88b526b07dcb7728a4df
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-01-05 23:54:42 +0100

    use Fusion style

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    a.setStyle(QStyleFactory::create("Fusion"));
     return a.exec();
 }
